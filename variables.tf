c4a0eb851dc132d6c4a568470caa9d07a3c233a6
Noa Ben zion <67430185+noa1608@users.noreply.github.com>
2024-12-09 23:41:21 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,27 +1,47 @@
+variable "region" {
+  default = "eu-west-3"
+}
+
 variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
 
-variable "public_subnet_cidr_a" {
-  default = "10.0.9.0/24"
+variable "public_subnet_1_cidr" {
+  default = "10.0.1.0/24"  # CIDR for public subnet 1
 }
 
-variable "public_subnet_cidr_b" {
-  default = "10.0.10.0/24" 
+variable "public_subnet_2_cidr" {
+  default = "10.0.2.0/24"  # CIDR for public subnet 2
 }
 
 variable "private_subnet_cidr" {
-  default = "10.0.11.0/24"
+  default = "10.0.3.0/24"
 }
 
-variable "availability_zone_a" {
-  default = "us-east-1a"
+variable "public_subnet_1_az" {
+  default = "eu-west-3a" 
 }
 
-variable "availability_zone_b" {
-  default = "us-east-1b"
+variable "public_subnet_2_az" {
+  default = "eu-west-3b"
 }
-variable "AWS_REGION" {
-  type    = string
-  default = "us-east-1" 
+
+variable "private_subnet_az" {
+  default = "eu-west-3a"
+}
+
+variable "instance_type" {
+  description = "EC2 Instance type"
+  default     = "t3.micro"
+}
+
+
+variable "ami_id" {
+  description = "AMI ID for Ubuntu 22.04 LTS"
+  default     = "ami-09be70e689bddcef5" 
+}
+
+variable "key_name" {
+  description = "Key pair name for EC2 access"
+  default     = "paris"  
 }
